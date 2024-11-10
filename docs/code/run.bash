9c2c1bf197dc4d85619ec13077c9d911ef867383
Francis Bond <bond@ieee.org>
2024-11-10 20:31:32 +0100

    use local environment for running

--- docs/code/run.bash
+++ docs/code/run.bash
@@ -1,13 +1,20 @@
+if [ -d "./venv" ]
+then
+    source ./venv/bin/activate
+    pip install -r requirements.txt
+else
+    python3.9 -m venv "./venv"
+    source ./venv/bin/activate
+    python -m pip install --upgrade pip
+    pip install -r requirements.txt
+fi
+
+
 #for prog in wk2a wk2b wk3a wk3b wk4a wk4b wk5a wk5b wk6a wk6b wk7a wk7b wk8a wk8b wk9a wk9b wk10a wk10b wk11b
-for prog in  wk07b 
+for prog in   wk09b
 do
     pygmentize -f html -O full -o ${prog}.html ${prog}.py
     python3 ${prog}.py > ${prog}-out.txt
-done
-
-### for testing
-for prog in test_wk07b
-do
-    pygmentize -f html -O full -o ${prog}.html ${prog}.py
-    pytest -v  ${prog}.py > ${prog}-out.txt
+    #pygmentize -f html -O full -o test_${prog}.html test_${prog}.py
+    #pytest -v  test_${prog}.py > test_${prog}-out.txt
 done
